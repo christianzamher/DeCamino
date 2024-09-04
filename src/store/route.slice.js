76dfc271b69503c111fb088cbf865ed8a95eaeaf
wulf <czh1983@gmail.com>
2024-09-04 20:32:55 -0300

    uploading files

---
+++ src/store/route.slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const routeSlice = createSlice({
+  name: 'route',
+  initialState: {
+    startLocation: [-34.6047, -58.3995],
+    endLocation: [-34.6036, -58.3985],
+  },
+  reducers: {
+    setStartLocation: (state, action) => {
+      state.startLocation = action.payload;
+    },
+    setEndLocation: (state, action) => {
+      state.endLocation = action.payload;
+    },
+  },
+});
+
+export const { setStartLocation, setEndLocation } = routeSlice.actions;
+
